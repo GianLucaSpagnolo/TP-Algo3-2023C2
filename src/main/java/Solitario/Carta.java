4e06d8889f35ef961fbaf0fc9e5a8a2197096276
GianLucaSpagnolo <gianluuba@gmail.com>
2023-09-14 20:25:55 -0300

    Inicio TP

---
+++ src/main/java/Solitario/Carta.java
@@ -0,0 +1,9 @@
+package Solitario;
+
+public class Carta {
+    private Integer numero;
+    private String color;
+    private String palo;
+    private boolean visible;
+    private boolean interactuar;
+}
