044f665bd937efdd0f0340d9b72a9605fa2c8e6c
MateoRequejo <marequejo@fi.uba.ar>
2023-09-15 14:42:16 -0600

    Modificacion Klondike

--- src/main/java/Reglas/Klondike/Klondike.java
+++ src/main/java/Reglas/Klondike/Klondike.java
@@ -1,7 +1,12 @@
 package Reglas.Klondike;
 
+import Solitario.Carta;
+
+import java.util.List;
+
 public class Klondike {
     private String regla;
     private String version;
+    private List<Carta> Mazo;
 
 }
