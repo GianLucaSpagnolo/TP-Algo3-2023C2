3c36fadb78bc1d77fbcafe4066bcfaf36bfd2399
MateoRequejo <marequejo@fi.uba.ar>
2023-09-19 14:36:37 -0600

    Primer avance Mazo

--- src/main/java/Reglas/Reglas.java
+++ src/main/java/Reglas/Reglas.java
@@ -2,12 +2,13 @@ package Reglas;
 
 import java.util.Stack;
 import Solitario.Carta;
+import Solitario.Mazo;
 import Solitario.Mesa;
 
 public interface Reglas {
     boolean moverCarta(Carta carta, Integer columna);
     boolean moverCartaColumnaFinal(Carta carta, Integer columna);
-    Stack inicializarCaraja();
+    Mazo inicializarCaraja();
     Mesa inicializarJuego(Mesa mesa);
     void repartirCartas();
     boolean estaGanado();
--- src/main/java/Solitario/Mazo.java
+++ src/main/java/Solitario/Mazo.java
@@ -1,4 +1,51 @@
 package Solitario;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Stack;
+
 public class Mazo {
+    private Stack<Carta> baraja;
+
+    public Mazo(Integer cantidad, List<Palos> palos) {
+        this.baraja = new Stack<>();
+        int contador = 0;
+        int limite = cantidad/palos.size();
+        for (Palos palo : palos) {
+            while (contador < limite) {
+                for (int i = 1; i < 14; i++) {
+                    Carta carta = new Carta(i, palo);
+                    baraja.push(carta);
+                    contador++;
+                }
+            }
+            contador = 0;
+        }
+    }
+
+    public void agregarCarta(Carta carta) {
+        baraja.push(carta);
+    }
+
+    public Carta sacarCarta() {
+        if (baraja.empty()) {
+            return null;
+        }
+        return baraja.pop();
+    }
+
+    public void mezclar() {
+        ArrayList<Carta> auxiliar = new ArrayList<>();
+        while (!baraja.empty()) {
+            auxiliar.add(sacarCarta());
+        }
+        Random generador = new Random();
+        int cantidad = auxiliar.size();
+        for (int i = 0; i < cantidad; i++) {
+            int indice = generador.nextInt(0, cantidad - i);
+            agregarCarta(auxiliar.get(indice));
+        }
+    }
+
 }
