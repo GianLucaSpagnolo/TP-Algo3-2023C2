fc7268f7cb25d8a3b0aebc517caa8a3d1a3639c1
MateoRequejo <marequejo@fi.uba.ar>
2023-11-10 14:01:33 -0600

    Avance etapa 3. VentanaJuego y ventanaInicial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,60 +1,25 @@
-import Reglas.Klondike;
-import Reglas.Solitario;
-import Reglas.Spider;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Accordion;
-import javafx.scene.control.Button;
-import javafx.scene.control.TitledPane;
-import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
-import ui.Vista.VistaJuego;
-import ui.Vista.VistaKlondike;
-import ui.Vista.VistaSpider;
 
 public class Main extends Application {
-    private Solitario modelo;
-    @FXML
-    Button botonKlondike;
-
-    @FXML
-    Button botonSpider;
+    @Override
+    public void start(Stage stage) throws Exception {
+        try {
+            Parent ventana = FXMLLoader.load((getClass().getResource("ventanaInicial.fxml")));
+            Scene escena = new Scene(ventana);
+            stage.setScene(escena);
+            stage.setTitle("Solitario");
+            stage.setResizable(false);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         launch();
     }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        var loaderVentanaInicial = new FXMLLoader(getClass().getResource("ventanaInicial.fxml"));
-        loaderVentanaInicial.setController(this);
-        FlowPane ventana = loaderVentanaInicial.load();
-
-        var loaderMesa = new FXMLLoader(getClass().getResource("Mesa.fxml"));
-        TitledPane mesa = loaderMesa.load();
-
-        botonKlondike.setOnAction(actionEvent -> {
-            modelo = new Klondike(null, null);
-            loaderMesa.setController(this);
-            var scene = new Scene(mesa, 600,600);
-            stage.setScene(scene);
-            stage.show();
-            VistaKlondike vista = new VistaKlondike(stage, modelo);
-        });
-
-        botonSpider.setOnAction(actionEvent -> {
-            modelo = new Spider(null, null);
-            VistaSpider vista = new VistaSpider(stage, modelo);
-        });
-
-        var scene = new Scene(ventana, 500, 300);
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.show();
-    }
-
 }
